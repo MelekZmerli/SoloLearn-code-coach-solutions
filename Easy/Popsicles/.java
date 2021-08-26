db4ba4f61ce76a58c405203f5fd703bf748ebcdd
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-08-26 01:05:12 +0100

    added popsicles solution in java

---
+++ Easy/Popsicles/.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class program{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int siblings = input.nextInt();
+    int popsicles = input.nextInt();
+    if (popsicles % siblings == 0){
+      System.out.println("give away");
+    }
+    else{
+      System.out.println("eat them yourself");
+    }
+  }
+}
