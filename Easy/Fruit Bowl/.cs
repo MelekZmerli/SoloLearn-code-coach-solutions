b8b8c5f451323a89aaf0f2ff8846d2950e33cdda
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-08-27 21:54:50 +0100

    added fruit bowl solution in c#

---
+++ Easy/Fruit Bowl/.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn{
+  class Program{
+    static void Main(String[] args){
+      int fruit = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(fruit/6);
+    }
+  }
+}
