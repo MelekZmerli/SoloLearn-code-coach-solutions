6e57be8df31cdc163d2b52a14fed94c84996bc32
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-08-27 22:01:12 +0100

    added fruit bowl solution in java

---
+++ Easy/Fruit Bowl/.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Program{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int fruit = input.nextInt();
+    System.out.println(fruit/6);
+  }
+}
