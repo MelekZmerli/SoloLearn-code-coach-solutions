fe8b7eb63ce63b5a01873fb83681d3d4a73db468
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-08-29 19:44:44 +0100

    added skee-ball solution in c#

---
+++ Easy/Skee-Ball/.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn{
+  class Program{
+    static void Main(string[] args){
+      int pts, gun;
+      pts = Convert.ToInt32(Console.ReadLine();
+      gun = Convert.ToInt32(Console.ReadLine();
+      if(pts/12>=gun){
+        Console.Write("Buy it!");
+      }
+      else Console.Write("Try again");
+    }
+  }
+}
