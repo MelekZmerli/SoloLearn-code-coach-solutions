01d732e8f42d2b216fd950a21564eb48969a4499
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-09-01 23:45:22 +0100

    added gotham city solution in swift

---
+++ Easy/Gotham City/.swift
@@ -0,0 +1,11 @@
+var crimes:Int?
+crimes = Int(readLine()!);
+if (crimes!<5){
+  print("I got this!");
+}
+else if (crimes! >= 5) && (crimes! < 10){
+  print("Help me Batman);
+}
+else{
+  print("Good Luck out there!");
+}
