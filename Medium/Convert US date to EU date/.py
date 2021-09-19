738fab1e39d23a11243318aa66ebfdefa34c9bf9
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-09-19 11:37:19 +0100

    created convert us date to eu date solution in python

---
+++ Medium/Convert US date to EU date/.py
@@ -0,0 +1,22 @@
+d = input()
+l = []
+months = {"January": "1",
+          "February": "2",
+          "March": "3",
+          "April": "4",
+          "May": "5",
+          "June": "6",
+          "July": "7",
+          "August": "8",
+          "September": "9",
+          "October": "10",
+          "November": "11",
+          "December": "12"
+}
+if '/' in d:
+  l = d.split('/')
+  print(l[1]+'/'+l[0]+'/'+l[2])
+else:
+  l = d.split(' ')
+  print(l[1][:len(l[1])-1]+'/'+months[l[0]]+'/'+l[2])
+  
