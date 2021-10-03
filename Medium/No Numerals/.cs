ee13fdd46261c7d2580af8590b445504fffce157
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-10-03 23:53:44 +0100

    added no numerals solution in c#

---
+++ Medium/No Numerals/.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SoloLearn{
+    class Program{
+        static void Main(String[] args){
+            Console.Write(Console.ReadLine().Replace("10","ten").Replace("0","zero").Replace("1","one").Replace("2","two").Replace("3","three").Replace("4","four").Replace("5","five").Replace("6","six").Replace("7","seven").Replace("8","eight").Replace("9","nine"));
+        }
+    }
+}
