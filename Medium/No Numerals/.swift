44b0e41435effa35abc5e2aa5c8a72307cd7926a
MelekZmerli <85803635+MelekZmerli@users.noreply.github.com>
2021-10-18 12:51:56 +0100

    added no numerals solution in swift

---
+++ Medium/No Numerals/.swift
@@ -0,0 +1,3 @@
+import Foundation
+let result = readLine()!.replacingOccurrences(of: "10",with: "ten").replacingOccurrences(of: "0",with: "zero").replacingOccurrences(of: "1",with: "one").replacingOccurrences(of: "2",with: "two").replacingOccurrences(of: "3",with: "three").replacingOccurrences(of: "4",with: "four").replacingOccurrences(of: "5",with: "five").replacingOccurrences(of: "6",with: "six").replacingOccurrences(of: "7",with: "seven").replacingOccurrences(of: "8",with: "eight").replacingOccurrences(of: "9",with: "nine");
+print(result);
