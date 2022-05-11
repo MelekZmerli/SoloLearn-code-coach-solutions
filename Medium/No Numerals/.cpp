ec6b9dd13213a534f901cd5c13beadedf4023b0c
Malek Zmerli <zmerlimelek@gmail.com>
2022-05-11 09:57:02 +0100

    fixed switch type

--- Medium/No Numerals/.cpp
+++ Medium/No Numerals/.cpp
@@ -7,7 +7,7 @@ int main(){
     getline(cin,input);
     for(int i = 0; i < input.length(); i++){
         switch(input[i]){
-            case 1:
+            case '1':
                 if(input[i+1] == '0'){
                     input.insert(i+2,"ten");
                     input.erase(i,2);
@@ -17,35 +17,35 @@ int main(){
                     input.erase(--i,1);
                 }
                 break;
-            case 2:
+            case '2':
                 input.insert(++i,"two");
                 input.erase(--i,1);
                 break;
-            case 3:
+            case '3':
                 input.insert(++i,"three");
                 input.erase(--i,1);
                 break;
-            case 4:
+            case '4':
                 input.insert(++i,"four");
                 input.erase(--i,1);
                 break;
-            case 5:
+            case '5':
                 input.insert(++i,"five");
                 input.erase(--i,1);
                 break;
-            case 6:
+            case '6':
                 input.insert(++i,"six");
                 input.erase(--i,1);
                 break;
-            case 7:
+            case '7':
                 input.insert(++i,"seven");
                 input.erase(--i,1);
                 break;
-            case 8:
+            case '8':
                 input.insert(++i,"eight");
                 input.erase(--i,1);
                 break;
-            case 9:
+            case '9':
                 input.insert(++i,"nine");
                 input.erase(--i,1);
                 break;
